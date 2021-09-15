08425d4a6686b785ba29dadb81b308a08dd4556f
Michael <michael.john.davis.1@gmail.com>
2021-09-15 17:21:39 +1200

    More cards
    - standard hearts and diamonds added to card list dictionary

--- CardList.gd
+++ CardList.gd
@@ -70,5 +70,130 @@ var card_dictionary = {
 		"name": "Ace of Clubs",
 		"suit": "clubs",
 		"value": "1"
+	},
+		"028": {
+		"name": "Two of Diamonds",
+		"suit": "diamonds",
+		"value": 2
+	},
+		"029": {
+		"name": "Three of Diamonds",
+		"suit": "diamonds",
+		"value": 3
+	},
+		"030": {
+		"name": "Four of Diamonds",
+		"suit": "diamonds",
+		"value": 4
+	},
+		"031": {
+		"name": "Five of Diamonds",
+		"suit": "diamonds",
+		"value": 5
+	},
+		"032": {
+		"name": "Six of Diamonds",
+		"suit": "diamonds",
+		"value": 6
+	},
+		"033": {
+		"name": "Seven of Diamonds",
+		"suit": "diamonds",
+		"value": 7
+	},
+		"034": {
+		"name": "Eight of Diamonds",
+		"suit": "diamonds",
+		"value": 8
+	},
+		"035": {
+		"name": "Nine of Diamonds",
+		"suit": "diamonds",
+		"value": 9
+	},
+		"036": {
+		"name": "Ten of Diamonds",
+		"suit": "diamonds",
+		"value": 10
+	},
+		"037": {
+		"name": "Jack of Diamonds",
+		"suit": "diamonds",
+		"value": "J"
+	},
+		"038": {
+		"name": "Queen of Diamonds",
+		"suit": "diamonds",
+		"value": "Q"
+	},
+		"039": {
+		"name": "King of Diamonds",
+		"suit": "diamonds",
+		"value": "K"
+	},
+		"040": {
+		"name": "Ace of Hearts",
+		"suit": "hearts",
+		"value": 1
+	},
+		"041": {
+		"name": "Two of Hearts",
+		"suit": "hearts",
+		"value": 2
+	},
+		"042": {
+		"name": "Three of Hearts",
+		"suit": "hearts",
+		"value": 3
+	},
+		"043": {
+		"name": "Four of Hearts",
+		"suit": "hearts",
+		"value": 4
+	},
+		"044": {
+		"name": "Five of Hearts",
+		"suit": "hearts",
+		"value": 5
+	},
+		"045": {
+		"name": "Six of Hearts",
+		"suit": "hearts",
+		"value": 6
+	},
+		"046": {
+		"name": "Seven of Hearts",
+		"suit": "hearts",
+		"value": 7
+	},
+		"047": {
+		"name": "Eight of Hearts",
+		"suit": "hearts",
+		"value": 8
+	},
+		"048": {
+		"name": "Nine of Hearts",
+		"suit": "hearts",
+		"value": 9
+	},
+		"049": {
+		"name": "Ten of Hearts",
+		"suit": "hearts",
+		"value": 10
+	},
+		"050": {
+		"name": "Jack of Hearts",
+		"suit": "hearts",
+		"value": "J"
+	},
+		"051": {
+		"name": "Queen of Hearts",
+		"suit": "hearts",
+		"value": "Q"
+	},
+		"052": {
+		"name": "King of Hearts",
+		"suit": "hearts",
+		"value": "K"
 	}
 }
